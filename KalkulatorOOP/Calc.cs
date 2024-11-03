677b85424d6ef8f5dced2eaa5cd14b3749dbafbb
ezegy <ezegy@DESKTOP-9POM430>
2024-11-03 19:40:21 +0100

    Itt van

---
+++ KalkulatorOOP/Calc.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalkulatorOOP
+{
+    internal class Calc
+    {
+        private double szám1;
+        private double szám2;
+        private char műveletJel;
+        private double eredmény;
+
+        
+        public Calc() { }
+
+        
+        public Calc(double szám1, double szám2, char műveletJel)
+        {
+            this.szám1 = szám1;
+            this.szám2 = szám2;
+            this.műveletJel = műveletJel;
+        }
+
+        
+        public void SetSzám1(double szám1)
+        {
+            this.szám1 = szám1;
+        }
+
+        
+        public void SetSzám2(double szám2)
+        {
+            this.szám2 = szám2;
+        }
+
+        
+        public void SetMűveletJel(char műveletJel)
+        {
+            this.műveletJel = műveletJel;
+        }
+
+        
+        public void SetEredmény()
+        {
+            switch (műveletJel)
+            {
+                case '+':
+                    eredmény = szám1 + szám2;
+                    break;
+                case '-':
+                    eredmény = szám1 - szám2;
+                    break;
+                case '*':
+                    eredmény = szám1 * szám2;
+                    break;
+                case '/':
+                    if (szám2 == 0)
+                    {
+                        throw new DivideByZeroException("Nullával nem lehet osztani!");
+                    }
+                    eredmény = szám1 / szám2;
+                    break;
+                default:
+                    throw new ArgumentException("Érvénytelen művelet jel!");
+            }
+        }
+
+        
+        public double GetEredmény()
+        {
+            return eredmény;
+        }
+    }
+}
--- KalkulatorOOP/Program.cs
+++ KalkulatorOOP/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using KalkulatorOOP;
+using System;
+
+namespace KalkulatorOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc calc = new Calc();
+            calc.SetSzám1(10);
+            calc.SetSzám2(3);
+            calc.SetMűveletJel('*');
+            calc.SetEredmény();
+            Console.WriteLine("Az eredmény: " + calc.GetEredmény());
+        }
+    }
+}
